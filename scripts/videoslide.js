24042b06a3cdd9b7d96f456ac909d9a3b7ba3034
w601sxs <shreyas.f117@gmail.com>
2018-07-26 09:43:06 -0400

    Add files via upload

---
+++ scripts/videoslide.js
@@ -0,0 +1,17 @@
+$('#main-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 300000,
+      dots: true,
+      infinite: true,
+      adaptiveHeight: true,
+      arrows: false
+  });
+
+  var video = $('#main-slider .slick-active').find('iframe').get(0).play();
+
+  $('#main-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('#main-slider .slick-slide').find('video').get(0).pause();
+    var video = $('#main-slider .slick-active').find('video').get(0).play();
+});
